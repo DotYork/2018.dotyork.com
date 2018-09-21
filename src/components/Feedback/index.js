82e55ab7772ed971adb89f5dfc509108122d3151
Rick Chadwick <rick@madeinbeta.co.uk>
2018-09-21 16:32:05 +0100

    section headers

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const Feedback= () => {
+const Feedback = () => {
   return (
-    <h1>Feedback</h1>
-  )
-}
+    <div className="p-view__header">
+      <h1>What Did You Think?</h1>
+      <p>
+        Your opinion really matters, it helps us figure out what we need to do
+        in the future with DotYork.
+      </p>
+      <p>
+        Complete all sections to be entered into a draw to win a £50
+        Amazon.co.uk voucher AND receive a free drink at the conference
+        after-party.
+      </p>
+    </div>
+  );
+};
 
 export default Feedback;
--- src/components/Help/index.js
+++ src/components/Help/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const Help= () => {
+const Help = () => {
   return (
-    <h1>Help</h1>
-  )
-}
+    <div className="p-view__header">
+      <h1>Get Help</h1>
+      <p>
+        If you need assistance, you can send us a text, email, message or DM at
+        any time.
+      </p>
+    </div>
+  );
+};
 
 export default Help;
