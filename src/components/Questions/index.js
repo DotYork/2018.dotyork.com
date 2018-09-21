8affdf7825b035ae7c0b9e0c76153aa0b119136d
Rick Chadwick <rick@madeinbeta.co.uk>
2018-09-21 13:41:53 +0100

    test netlify form

--- src/components/Questions/index.js
+++ src/components/Questions/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 const Questions= () => {
   return (
-    <h1>Questions</h1>
+    <div>
+      <h1>Questions</h1>
+
+      <form name="question" method="post">
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label>Your Name: <input type="text" name="name"/></label>
+        </p>
+        <p>
+          <label>Question: <textarea name="question"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </div>
   )
 }
 
