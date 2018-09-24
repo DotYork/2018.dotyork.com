8d9780d2609e49796d36dbfa9730e1310d1e6a17
Rick Chadwick <rick@madeinbeta.co.uk>
2018-09-24 16:13:19 +0100

    twitter intent

--- src/components/Schedule/SpeakerDetail.js
+++ src/components/Schedule/SpeakerDetail.js
@@ -1,29 +1,39 @@
 //  Get speaker content
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import Twitter from '../../assets/icons/twitter.js';
+import React from "react";
+import PropTypes from "prop-types";
+import Twitter from "../../assets/icons/twitter.js";
 
-const SpeakerDetail = (props) => {
-  const twitter = (username) => {
+const SpeakerDetail = props => {
+  const twitterLinkText = username => {
     if (username) {
-      return `Tweet @${username}`
+      return `Tweet @${username}`;
     }
-    return 'Tweet';
-  }
-  
+    return "Tweet";
+  };
+
+  const twitterURL = username => {
+    if (username) {
+      return `https://twitter.com/intent/tweet?via=${username}&hashtags=DotYork`;
+    }
+    return "https://twitter.com/intent/tweet?hashtags=DotYork";
+  };
+
   return (
-    <div className="p-speaker__body"> 
-      <a href="" className="b-btn p-speaker__twitter-btn">
+    <div className="p-speaker__body">
+      <a
+        href={twitterURL(props.speaker_twitter)}
+        className="b-btn p-speaker__twitter-btn"
+      >
         <Twitter fill="#0C9D5E" />
-        {twitter(props.speaker_twitter)}
+        {twitterLinkText(props.speaker_twitter)}
       </a>
     </div>
-  )
-}
+  );
+};
 
 SpeakerDetail.propTypes = {
   speaker_twitter: PropTypes.string
-}
+};
 
-export default SpeakerDetail
+export default SpeakerDetail;
