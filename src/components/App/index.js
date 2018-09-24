adcc8d85e93834275e73cb066b04860b5df91e3c
Rick Chadwick <rick@madeinbeta.co.uk>
2018-09-24 15:33:35 +0100

    initial load to schedule

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,35 +1,32 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  Route,
-  Switch
-} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "./index.css";
 
 //  App components
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
-import Schedule from '../../components/Schedule';
-import Questions from '../../components/Questions';
-import Feedback from '../../components/Feedback';
-import Help from '../../components/Help';
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import Schedule from "../../components/Schedule";
+import Questions from "../../components/Questions";
+import Feedback from "../../components/Feedback";
+import Help from "../../components/Help";
 
-const App = ()=> (
+const App = () => (
   <BrowserRouter>
     <div className="b-container">
       <Header />
 
       <Switch>
+        <Route exact path="/" render={() => <Redirect to="/schedule" />} />
         <Route path="/schedule" component={Schedule} />
         <Route path="/questions" component={Questions} />
         <Route path="/feedback" component={Feedback} />
         <Route path="/help" component={Help} />
       </Switch>
-      
+
       <Footer />
     </div>
   </BrowserRouter>
-)
+);
 
 export default App;
