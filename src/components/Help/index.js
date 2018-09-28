862b5c5f57f618eeb97837934b9713b0e81a75d7
Rick Chadwick <rick@madeinbeta.co.uk>
2018-09-28 15:04:09 +0100

    some help content

--- src/components/Help/index.js
+++ src/components/Help/index.js
@@ -15,6 +15,76 @@ const Help = () => {
             DotYork team at any time throughout the day.
           </p>
         </div>
+
+        <div className="p-help-section">
+          <h2 className="p-help-section__title">Finding Us</h2>
+          <div className="p-help-section__body">
+            <p>
+              DotYork staff will be wearing purple DotYork t-shirts and have
+              staff lanyards. If you need help at any time, ask a member of
+              staff.
+            </p>
+            <p>
+              No-one in sight? Try the registration desk in The Hospitium for
+              help throughout the day.
+            </p>
+          </div>
+        </div>
+
+        <div className="p-help-section">
+          <h2 className="p-help-section__title">Contact Us</h2>
+          <div className="p-help-section__body">
+            <ul>
+              <li>
+                <a href="mailto:help@dotyork.com">
+                  <b>Email</b> help@dotyork.com
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/dot_york">
+                  <b>Tweet</b> @dot_york
+                </a>{" "}
+                (DM’s are open)
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="p-help-section">
+          <h2 className="p-help-section__title">Toilets</h2>
+          <div className="p-help-section__body">
+            <p>
+              Toilets are located in The Hospitium. They have been liberated
+              from the gender-binary.
+            </p>
+          </div>
+        </div>
+
+        <div className="p-help-section">
+          <h2 className="p-help-section__title">Need to get away?</h2>
+          <div className="p-help-section__body">
+            <p>
+              If you need some quiet time during sessions, there is a room to
+              the left of the stage.
+            </p>
+          </div>
+        </div>
+
+        <div className="p-help-section">
+          <h2 className="p-help-section__title">Photography</h2>
+          <div className="p-help-section__body">
+            <p>
+              This event is being photographed by Hewitt & Walker for our future
+              marketing. If you’d rather we didn’t use/share any photos of you,
+              you can turn around your name badge to the{" "}
+              <b>'Please don’t share photos of me'</b> side.
+            </p>
+            <p>
+              We can’t promise that we won’t <i>take</i> photos of you but we
+              won’t share/publish them anywhere.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
